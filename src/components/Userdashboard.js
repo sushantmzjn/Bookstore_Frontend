a3e9727d271ba2fba3797847ed84604fe4ee211f
sushantmzjn <[sushantmzjn@gmail.com]>
2020-02-12 20:09:20 +0545

    user dashboard

--- src/components/Userdashboard.js
+++ src/components/Userdashboard.js
@@ -4,7 +4,7 @@ import { Route } from "react-router-dom";
 import Image1 from "./images/book.jpg";
 import Image2 from "./images/book2.jpg"
 import { Carousel } from "react-bootstrap";
-
+import './userdashboard.css';
 
 export default class Userdashboard extends Component {
     constructor(props) {
@@ -12,12 +12,10 @@ export default class Userdashboard extends Component {
     }
 
     render() {
-
         return (
             <React.Fragment>
                 <Route component={UserHeader} />
-
-
+                <Carousel className="maindiv"></Carousel>
                 <p>user dash</p>
             </React.Fragment>
         );
