7e659335501de1ddfb7aacdfe7b778b72ee01916
sushantmzjn <[sushantmzjn@gmail.com]>
2020-02-12 13:08:25 +0545

    login

--- src/components/login.js
+++ src/components/login.js
@@ -1,9 +1,12 @@
 import React from "react";
+import {Route} from "react-router-dom";
 import { Form, Button } from "react-bootstrap";
 import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBInput } from 'mdbreact';
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
 import axios from "axios";
+import Header from "./Header";
+
 
 export default class Login extends React.Component {
     constructor(props) {
@@ -40,22 +43,25 @@ export default class Login extends React.Component {
 
     render() {
         return (
-            <MDBContainer>
-                <MDBRow>
-                    <MDBCol md="4">
-                        <form onSubmit={this.loginHandler}>
-                            <p className="h5 text-center mb-4">Sign in</p>
-                            <div className="grey-text">
-                                <MDBInput label="Type your username" type="text" onChange={this.usernameHandler} required />
-                                <MDBInput label="Type your password" type="password" onChange={this.passwordHandler} required />
-                            </div>
-                            <div className="text-center">
-                                <Button type="submit" color="primary" onClick={this.login}>Login</Button>
-                            </div>
-                        </form>
-                    </MDBCol>
-                </MDBRow>
-            </MDBContainer>
+            <React.Fragment>
+                <Route component={Header} />
+                <MDBContainer>
+                    <MDBRow>
+                        <MDBCol md="4">
+                            <form onSubmit={this.loginHandler}>
+                                <p className="h5 text-center mb-4">Sign in</p>
+                                <div className="grey-text">
+                                    <MDBInput label="Type your username" type="text" onChange={this.usernameHandler} required />
+                                    <MDBInput label="Type your password" type="password" onChange={this.passwordHandler} required />
+                                </div>
+                                <div className="text-center">
+                                    <Button type="submit" color="primary" onClick={this.login}>Login</Button>
+                                </div>
+                            </form>
+                        </MDBCol>
+                    </MDBRow>
+                </MDBContainer>
+            </React.Fragment>
         )
     }
 
