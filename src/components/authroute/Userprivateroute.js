0b0b13c73b81714e655579221e6363d324ff8f88
sushantmzjn <[sushantmzjn@gmail.com]>
2020-02-15 19:57:31 +0545

    route

--- src/components/authroute/Userprivateroute.js
+++ src/components/authroute/Userprivateroute.js
@@ -9,7 +9,7 @@ const PrivateRoute = ({ component: Component, ...rest }) => (
                 <Component {...props} />
             ) : (
                     <Redirect to={{
-                        pathname: '/',
+                        pathname: '/signup',
                         state: { from: props.location }
                     }}
                     />
